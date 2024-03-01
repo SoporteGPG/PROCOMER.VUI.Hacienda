5d714dbc826d1888dc5a18d275ff7de30a8370b4
SoporteGPG <123422894+SoporteGPG@users.noreply.github.com>
2024-03-01 09:42:44 -0600

    metodo

--- ServicioRUT/Services.asmx.cs
+++ ServicioRUT/Services.asmx.cs
@@ -22,8 +22,7 @@ namespace ServicioRUT
     public class Services : System.Web.Services.WebService
     {
 
-        
-[WebMethod]
+        [WebMethod]
         public string RealizarInscripcionJson(string pm_RefBase, string pm_Ambiente)
         {
             SqlCommand cmdTabla = new SqlCommand();
@@ -32,12 +31,12 @@ namespace ServicioRUT
             WS_ServicesRUTPruebas.ProcomerReq Datos = new WS_ServicesRUTPruebas.ProcomerReq();// Almacena los datos par enviarlos al servicio
             List<WS_ServicesRUTPruebas.ProcomerReq> DatosEmpresa = new List<WS_ServicesRUTPruebas.ProcomerReq>(); //Almacena en crudo los datos de la base de datos 
             string json = "";
-            //try
-            //{
+            try
+            {
                 SqlDataReader dr;
                 cmdTabla.CommandType = CommandType.StoredProcedure;
                 cmdTabla.CommandText = "sp_ConsultarInfo_RUT";
-                cmdTabla.Connection = objConexion.m_ObtenerConexionAmbiente(pm_Ambiente);
+                cmdTabla.Connection = objConexion.m_ObtenerConexionAmbiente();
                 cmdTabla.Parameters.Add("@RefBase", SqlDbType.NVarChar).Value = pm_RefBase;
                 dr = cmdTabla.ExecuteReader();
                 while (dr.Read())
@@ -46,82 +45,82 @@ namespace ServicioRUT
                     {
                         ActividadEconomica = new WS_ServicesRUTPruebas.ActividadEconomica()
                         {
-                            CodigoActividad = dr["ActCodigoActividad"].ToString(),
-                            NombreComercial = dr["ActNombreComercialRUT"].ToString(),
-                            FechaInicio = dr["ActFechaInicioRUT"].ToString(),
-                            TelefonoFijo = dr["ActTelefonoFijoPersonaRUT"].ToString(),
-                            NroProvincia = (short)Convert.ToInt32(dr["ActNroProvincia"].ToString()),
-                            StrProvincia = dr["ActStrProvincia"].ToString(),
-                            NroCanton = (short)Convert.ToInt32(dr["ActNroCanton"].ToString()),
-                            StrCanton = dr["ActStrCanton"].ToString(),
-                            NroDistrito = (short)Convert.ToInt32(dr["ActNroDistrito"].ToString()),
-                            StrDistrito = dr["ActStrDistrito"].ToString(),
-                            OtrasSenas = dr["ActOtrasSenas"].ToString()
+                            CodigoActividad = dr["3_ActCodigoActividad"].ToString(),
+                            NombreComercial = dr["3_Nombre comercial RUT"].ToString(),
+                            FechaInicio = dr["3_Fecha inicio RUT"].ToString(),
+                            TelefonoFijo = dr["3_Teléfono fijo persona RUT"].ToString(),
+                            NroProvincia = (short)Convert.ToInt32(dr["3_ActNroProvincia"].ToString()),
+                            StrProvincia = dr["3_ActStrProvincia"].ToString(),
+                            NroCanton = (short)Convert.ToInt32(dr["3_ActNroCanton"].ToString()),
+                            StrCanton = dr["3_ActStrCanton"].ToString(),
+                            NroDistrito = (short)Convert.ToInt32(dr["3_ActNroDistrito"].ToString()),
+                            StrDistrito = dr["3_ActStrDistrito"].ToString(),
+                            OtrasSenas = dr["3_ActOtrasSenas"].ToString()
                         },
                         Apoderado = new WS_ServicesRUTPruebas.Apoderado()
                         {
-                            NroIdentificacion = dr["ApdNroIdentificacion"].ToString(),
-                            CodTipoClasePersona = dr["ApdCodTipoClasePersona"].ToString(),
-                            NombreApoderado = dr["ApdNombreApoderado"].ToString(),
-                            NroProvincia = (short)Convert.ToInt32(dr["ApdNroProvincia"].ToString()),
-                            StrProvincia = dr["ApdStrProvincia"].ToString(),
-                            NroCanton = (short)Convert.ToInt32(dr["ApdNroCanton"].ToString()),
-                            StrCanton = dr["ActStrCanton"].ToString(),
-                            NroDistrito = (short)Convert.ToInt32(dr["ApdNroDistrito"].ToString()),
-                            StrDistrito = dr["ApdStrDistrito"].ToString(),
-                            OtrasSenas = dr["ApdOtrasSenas"].ToString(),
-                            Correo = dr["ApdCorreo"].ToString(),
-                            DetallePoder = dr["ApdDetallePoder"].ToString(),
-                            TelefonoMovil = dr["ApdTelefonoMovil"].ToString(),
-                            FechaInicio = dr["ApdFechaInicio"].ToString()
+                            NroIdentificacion = dr["3_ApdNroIdentificacion"].ToString(),
+                            CodTipoClasePersona = dr["3_ApdCodTipoClasePersona"].ToString(),
+                            NombreApoderado = dr["3_ApdNombreApoderado"].ToString(),
+                            NroProvincia = (short)Convert.ToInt32(dr["3_ApdNroProvincia"].ToString()),
+                            StrProvincia = dr["3_ApdStrProvincia"].ToString(),
+                            NroCanton = (short)Convert.ToInt32(dr["3_ApdNroCanton"].ToString()),
+                            StrCanton = dr["3_ApdStrCanton"].ToString(),
+                            NroDistrito = (short)Convert.ToInt32(dr["3_ApdNroDistrito"].ToString()),
+                            StrDistrito = dr["3_ApdStrDistrito"].ToString(),
+                            OtrasSenas = dr["3_ApdOtrasSenas"].ToString(),
+                            Correo = dr["3_ApdCorreo"].ToString(),
+                            DetallePoder = dr["3_ApdDetallePoder"].ToString(),
+                            TelefonoMovil = dr["3_ApdTelefonoMovil"].ToString(),
+                            FechaInicio = dr["3_ApdFechaInicio"].ToString()
                         },
                         Persona = new WS_ServicesRUTPruebas.Persona()
                         {
-                            NroIdentificacion = dr["PerNroIdentificacion"].ToString(),
-                            CodTipoClasePersona = dr["PerCodTipoClasePersona"].ToString(),
-                            NombreContribuyente = dr["PerNombreContribuyente"].ToString(),
-                            TelefonoFijo = dr["PersonaTelefonoFijo"].ToString(),
-                            TelefonoMovil = dr["PersonaTelefonoMovilRUT"].ToString(),
-                            Correo = dr["PersonaCorreoRUT"].ToString(),
-                            NroProvincia = (short)Convert.ToInt32(dr["PerNroProvincia"].ToString()),
-                            StrProvincia = dr["PerStrProvincia"].ToString(),
-                            NroCanton = (short)Convert.ToInt32(dr["PerNroCanton"].ToString()),
-                            StrCanton = dr["PerStrCanton"].ToString(),
-                            NroDistrito = (short)Convert.ToInt32(dr["PerNroDistrito"].ToString()),
-                            StrDistrito = dr["PerStrDistrito"].ToString(),
-                            OtrasSenas = dr["PerOtrasSenas"].ToString(),
-                            FechaCierre = dr["PersonaFechaCierre"].ToString()
+                            NroIdentificacion = dr["3_PerNroIdentificacion"].ToString(),
+                            CodTipoClasePersona = dr["3_PerCodTipoClasePersona"].ToString(),
+                            NombreContribuyente = dr["3_PerNombreContribuyente"].ToString(),
+                            TelefonoFijo = dr["3_PersonaTelefonoFijo"].ToString(),
+                            TelefonoMovil = dr["3_Persona teléfono movil RUT"].ToString(),
+                            Correo = dr["3_Persona correo RUT"].ToString(),
+                            NroProvincia = (short)Convert.ToInt32(dr["3_PerNroProvincia"].ToString()),
+                            StrProvincia = dr["3_PerStrProvincia"].ToString(),
+                            NroCanton = (short)Convert.ToInt32(dr["3_PerNroCanton"].ToString()),
+                            StrCanton = dr["3_PerStrCanton"].ToString(),
+                            NroDistrito = (short)Convert.ToInt32(dr["3_PerNroDistrito"].ToString()),
+                            StrDistrito = dr["3_PerStrDistrito"].ToString(),
+                            OtrasSenas = dr["3_PerOtrasSenas"].ToString(),
+                            FechaCierre = dr["3_PersonaFechaCierre"].ToString()
                         },
                         Representante = new WS_ServicesRUTPruebas.Representante
                         {
-                            NumIdentificacion = dr["RepNumIdentificacion"].ToString(),
-                            CodTipoClasePersona = dr["RepCodTipoClasePersona"].ToString(),
-                            StrNombreRepresentante = dr["RepStrNombreRepresentante"].ToString(),
-                            NroProvincia = (short)Convert.ToInt32(dr["RepNroProvincia"].ToString()),
-                            StrProvincia = dr["RepStrProvincia"].ToString(),
-                            NroCanton = (short)Convert.ToInt32(dr["RepNroCanton"].ToString()),
-                            StrCanton =dr["RepStrCanton"].ToString(),
-                            NroDistrito = (short)Convert.ToInt32(dr["RepNroDistrito"].ToString()),
-                            StrDistrito = dr["RepNroDistrito"].ToString(),
-                            OtrasSenas = dr["RepOtrasSenas"].ToString(),
-                            Correo = dr["RepCorreo"].ToString(),
-                            DetalleRepresentacion = dr["RepDetalleRepresentacion"].ToString(),
-                            TelefonoMovil = dr["RepTelefonoMovil"].ToString(),
-                            FechaInicio = dr["RepFechaInicio"].ToString(),
-                        }                     
+                            NumIdentificacion = dr["3_RepNumIdentificacion"].ToString(),
+                            CodTipoClasePersona = dr["3_RepCodTipoClasePersona"].ToString(),
+                            StrNombreRepresentante = dr["3_RepStrNombreRepresentante"].ToString(),
+                            NroProvincia = (short)Convert.ToInt32(dr["3_RepNroProvincia"].ToString()),
+                            StrProvincia = dr["3_RepStrProvincia"].ToString(),
+                            NroCanton = (short)Convert.ToInt32(dr["3_RepNroCanton"].ToString()),
+                            StrCanton = dr["3_RepStrCanton"].ToString(),
+                            NroDistrito = (short)Convert.ToInt32(dr["3_RepNroDistrito"].ToString()),
+                            StrDistrito = dr["3_RepStrDistrito"].ToString(),
+                            OtrasSenas = dr["3_RepOtrasSenas"].ToString(),
+                            Correo = dr["3_RepCorreo"].ToString(),
+                            DetalleRepresentacion = dr["3_RepDetalleRepresentacion"].ToString(),
+                            TelefonoMovil = dr["3_RepTelefonoMovil"].ToString(),
+                            FechaInicio = dr["3_RepFechaInicio"].ToString(),
+                        }
                     };
                     Datos = request2;
                 }
                 json = JsonConvert.SerializeObject(Datos);
 
                 WS_ServicesRUTPruebas.IInscripcionRUT rut = new WS_ServicesRUTPruebas.InscripcionRUTClient();
+                rut.RealizaInscripcionJson(json);
+                //WS_ServicesRUTPruebas.ProcomerRes res = new WS_ServicesRUTPruebas.ProcomerRes();
+            }
+            catch (Exception ex)
+            {
 
-                rut.RealizaInscripcionJson("");
-            //}
-            //catch (Exception ex)
-            //{
-
-            //}
+            }
             return json;
         }
 
